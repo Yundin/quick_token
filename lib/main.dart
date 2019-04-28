b464b99f75a64e0fb93f84e5f44a2eb067b6ac7c
Vladislav Yundin <4yundin@gmail.com>
2019-04-28 20:23:27 +0300

    Title changed

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Quick Token',
+      title: 'QuickToken',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
